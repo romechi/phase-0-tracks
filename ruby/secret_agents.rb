09e58e2716c82086f554679db7f1bec33387724d
romechi <dishmanj@gmail.com>
2017-07-22 15:41:10 -0500

    Update secret agent with driver code

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,3 +1,5 @@
+#release2
+
 #identify how long your string is
 #Set varible for first letter
 #change variable into the next letter of the alphabet
@@ -7,18 +9,21 @@
 def encrypt(secret_password)
   index = 0
 while index < secret_password.length
- secret_password[index] = secret_password[index].next!
-    index += 1
+  if secret_password.index"z"
+      secret_password[index] = "a"
+    else
+  secret_password[index] = secret_password[index].next!
 end
+    index += 1
+  end
 puts secret_password
 end
 
 encrypt("summer")
 
-
 #decrypt method shifts the letters back into original order
-#
-#
+#Set varible for first letter
+#Set a loop until the index is the same variable as the secret password length
 
 def decrypt(secret_password)
   index = 0
@@ -34,10 +39,41 @@ end
 
 decrypt("tvnnfs")
 
+#release 3
+puts encrypt("abc")
+puts encrypt("zed")
+puts decrypt("bcd")
+puts decrypt("afe")
+
+#release 4
+#decrypt(encrypt("swordfish"))
 
 
+#release 5
+# ask the user for their password
+puts "Would you like to decrypt (d) of encrypt (e) a password?"
+user_selection = gets.chomp
+
+#question2
+# user selects to decrypt or encrypt password
+
+  if user_selection == "d"
+    puts "Enter a password to decrypt"
+    user_password = gets.chomp
+    puts decrypt(user_password)
+    puts "Your password is #{user_password}"
 
 
+    else user_selection == "e"
+    puts "Enter password to encrypt"
+    user_password = gets.chomp
+    encrypt(user_password)
+
+#question3
+#user password is returned
+
+  puts "Your password is #{user_password}"
+end
 
 
 
