3c640c0e540b29f8b26227bbefcef34f370b96fc
romechi <dishmanj@gmail.com>
2017-07-23 18:54:48 -0500

    add pseudo code for vampires

---
+++ ruby/vampires.rb
@@ -0,0 +1,19 @@
+
+#Ask the following
+#Whats your name?
+#How old are you?
+#What year were you born?
+#What garlic bread? yes or no
+#Would you like to enroll in the company's health insurance? yes or no
+
+
+#release2 detection logic
+#if emloyee gave age, eats garlic bread and signs up for insurance return, puts "probably not a vampire"
+
+#if employee got age wrong, doesn't eat garlic bread and/or selects no for insurance return puts "probably a vampire"
+
+#if employee got age wrong, hates garlic bread and doesn't want insurance return puts "almost certainly a vampire"
+
+#if employee is amazing liar, anyone named "Drake Cula" or Tu Fang" return puts "devinitely a vampire"
+
+#otherwire print "results inconclusive"
