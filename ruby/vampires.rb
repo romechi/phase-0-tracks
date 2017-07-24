161d5d34732374b28fc5e949924ccde01b25c871
romechi <dishmanj@gmail.com>
2017-07-24 01:13:35 -0500

    Add questions for release 1

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,8 +3,31 @@
 #Whats your name?
 #How old are you?
 #What year were you born?
-#What garlic bread? yes or no
+#Do you like garlic bread? yes or no
 #Would you like to enroll in the company's health insurance? yes or no
+
+  puts "What is your name?"
+  person_name = gets.chomp
+
+  puts "How old are you?"
+  age = gets.chomp.to_i
+
+  puts "What year were you born?"
+  birth_year = gets.chomp.to_i
+
+  puts "The cafeteria serves garlic bread. Should we order some for you?"
+  garlic_bread = gets.chomp
+
+  puts "Would you like to enroll in the company's health insurance? Please answer with yes or no"
+  work_insurance = gets.chomp
+
+  if work_insurance == "yes"
+  puts "Cool you get insurance"
+
+    elsif work_insurance == "no"
+    puts "Why don't you want insurance?"
+
+   end
 
 
 #release2 detection logic
